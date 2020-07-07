504175b37564a747ffa997a727365f3c73b8512d
Vivek Patil <vvp8212@rit.edu>
2020-07-06 21:56:05 -0400

    Added Shopping List component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AppNavbar from './components/AppNavbar';
+import ShopplingList from './components/ShoppingList';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <AppNavbar/>
+      <ShopplingList/>
     </div>
   );
 }
