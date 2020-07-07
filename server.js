4556271eedb5a433628ff8fd7b0e219d1a3f69aa
Vivek Patil <vvp8212@rit.edu>
2020-07-07 17:01:07 -0400

    Deploying to heroku...

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -17,7 +17,16 @@ mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => console.log('Vivek, your MongoDB is connected..!'))
     .catch(err => console.log(err)); 
 
-app.use('/api/items', items);     
+app.use('/api/items', items);    
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000; 
 
